565569454ec73751ee7fe9459c67706a8e2e17c3
Mike Colbert <michael-colbert@uiowa.edu>
2022-09-25 19:43:49 -0500

    first push

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # iot-temperature
+iot-bais.azurewebsites.net
+
+
 Temperature and humidity API
+
+Database-driven, object-oriented
+
+Clone repository`
+
+Create a new virtual environment
+
+Windows: python -m venv ./venv
+Mac: python3 -m venv ./venv
+Activate the new virtual environment
+
+Windows: .\venv\Scripts\activate
+Mac: source ./venv/bin/activate
+Install the dependencies pip install -r requirements.txt
+
+Run application
+Windows: python app.py
+Mac: python3 app.py
+
+
+
+Application Requirements:
+Available in requirements.txt, but includes:
+MySQL Connector Python
---
+++ app.py
@@ -0,0 +1,77 @@
+import mysql.connector
+from mysql.connector import Error
+from flask import Flask
+from flask import request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+dbuser = os.getenv('DBUSER')
+dbpass = os.getenv('DBPASS')
+dbhost = os.getenv('DBHOST')
+dbname = os.getenv('DBNAME')
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Temperature API"
+
+@app.route('/temperature-data/', methods=['GET', 'POST'])
+def temperatures():
+    if request.method == 'GET':
+        conn = mysql.connector.connect(host=dbhost, database=dbname, user=dbuser, password=dbpass)
+        cur = conn.cursor()
+
+        sql = "SELECT * FROM temperatureLog ORDER BY readTime DESC"
+        cur.execute(sql)
+        print(sql)
+        results = cur.fetchall()
+
+        print("----------")
+        print("Total number of rows in table: ", cur.rowcount)
+        print("----------")
+        print(results)
+        
+        cur.close()
+        conn.close()
+            
+        return (results)
+
+    if request.method == 'POST':
+        form = request.form
+        sensorId = form['sensor']
+        temperature = form['temp']
+        humidity = form['humidity']
+        
+        conn = mysql.connector.connect(host=dbhost, database=dbname, user=dbuser, password=dbpass)
+        cur = conn.cursor()
+
+        sql = "INSERT INTO temperatures(readTime, sensorId, temperature, humidity) VALUES(NOW(), %s, %s, %s)",(sensorId, temperature, humidity)
+        cur.execute(sql)
+        print(sql)
+        results = cur.fetchall()
+
+        print("----------")
+        print("Total number of rows in table: ", cur.rowcount)
+        print("----------")
+        print(results)
+        
+        cur.close()
+        conn.close()
+            
+        return (results)
+
+
+@app.route('/temperature-data/sensors')
+def sensors():
+    return "Sensors"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
