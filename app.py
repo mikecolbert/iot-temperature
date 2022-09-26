bde4d014e6012f33e32b279b8f399e4b5887bdb5
Mike Colbert <michael-colbert@uiowa.edu>
2022-09-26 13:23:16 -0500

    xx

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 
 from flask import Flask
 from flask import request
-#from dotenv import load_dotenv
 import os
-
-#load_dotenv()
 
 app = Flask(__name__)
 
-#dbuser = os.getenv('DBUSER')
-#dbpass = os.getenv('DBPASS')
+dbuser = os.environ.get('DBUSER')
+dbpass = os.environ.get('DBPASS')
 dbhost = os.environ.get('DBHOST')
-#dbname = os.getenv('DBNAME')
+dbname = os.environ.get('DBNAME')
 
-#app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-
-#print(dbuser)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 @app.route('/')
 def index():
@@ -24,7 +19,7 @@ def index():
 
 @app.route('/temperature-data/sensors')
 def sensors():
-    return dbhost
+    return dbname
 
 
 if __name__ == '__main__':
